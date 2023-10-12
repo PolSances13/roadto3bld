8b084b1ea633e3432ce1cf7707bcce027f71a96c
PolSances13 <polsances@inslc.cat>
2023-10-12 12:11:55 +0200

    Exemple de Nav amb Contactt button de prova

--- actions.js
+++ actions.js
@@ -1,9 +1,1 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    const header = document.querySelector("header");
-    const changeColorButton = document.getElementById("changeColorButton");
-
-    changeColorButton.addEventListener("click", function() {
-        header.style.backgroundColor = "red"; // Cambia el color de fondo a rojo al hacer clic
-    });
-});
